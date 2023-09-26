212b9bd4d8bece6b33c4ff4a907977112cb2107d
Diego Hernandez <diegohxxiv@gmail.com>
2023-09-25 20:21:23 -0700

    Test

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,18 +1,22 @@
-import { fetchRecipes } from "@/lib/requests";
-import { RecipeBlock } from "@/components";
-import { Recipe } from "@/types";
+// import { fetchRecipes } from "@/lib/requests";
+// import { RecipeBlock } from "@/components";
+// import { Recipe } from "@/types";
 
-export default async function Page() {
-  const data: Recipe[] = await fetchRecipes();
+// export default async function Page() {
+//   const data: Recipe[] = await fetchRecipes();
 
-  return (
-    <div className="w-[90vw] mx-auto py-10 flex flex-col gap-10">
-      <p className="text-2xl font-[600]">Featured</p>
-      <div className="w-full grid place-items-start gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
-        {data?.map((recipe) => (
-          <RecipeBlock key={recipe?.id} {...recipe} />
-        ))}
-      </div>
-    </div>
-  );
+//   return (
+//     <div className="w-[90vw] mx-auto py-10 flex flex-col gap-10">
+//       <p className="text-2xl font-[600]">Featured</p>
+//       <div className="w-full grid place-items-start gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5">
+//         {data?.map((recipe) => (
+//           <RecipeBlock key={recipe?.id} {...recipe} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+export default function Page() {
+  return <p>Page</p>;
 }
